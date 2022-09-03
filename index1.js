d37cb223a7be2a5d9fc68e931b65b87296e31865
MajinDan <95249871+MajinDan@users.noreply.github.com>
2022-09-03 14:42:17 +0100

    java script lesson 3 finshed

---
+++ index1.js
@@ -0,0 +1,15 @@
+//find the nth character
+let firstName = "LoveLace";
+
+let thirdLetterOfLastName = firstName[2];
+/*bracket Notation to find Last Character
+ in string*/
+ let lastName = "LoveLace";
+
+let lastLetterOfLastName = LastName[lastName.length - 1];
+
+//find nth to last character in string
+
+let Name = "LoveLace";
+
+let secondtolastLetterOfLastName = Name[Name.length - 2];
---
+++ index2.js
@@ -0,0 +1,8 @@
+//word blanks
+
+function wordblanks(myNoun, myAdjective, myVerb)
+
+let result = "";
+result += "the " + myAdjective + myNoun + myVerb;
+return result;
+console.log(wordblanks("dog", "big", "quickly"))
